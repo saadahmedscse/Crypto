fa34fa812a74f9d916e6d754b37c6aaaa28c409e
Saad <saadahmedscse@gmail.com>
2022-10-08 17:01:06 +0600

    Supported Algorithms

---
+++ crypto/src/main/java/com/saadahmedsoft/crypto/utils/Algorithm.java
@@ -0,0 +1,10 @@
+package com.saadahmedsoft.crypto.utils;
+
+public class Algorithm {
+    public static String SHA1 = "SHA1";
+    public static String SHA224 = "SHA224";
+    public static String SHA256 = "SHA256";
+    public static String SHA384 = "SHA384";
+    public static String SHA512 = "SHA512";
+    public static String MD5 = "MD5";
+}
